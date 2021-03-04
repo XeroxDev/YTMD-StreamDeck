98d6ac61e82eb604142de08b705cd4b272564642
XeroxDev <admin@xeroxdev.de>
2021-03-04 13:00:00 +0100

    feat: add localization support for PI

---
+++ src/interfaces/localization.interface.ts
@@ -0,0 +1,10 @@
+export interface LocalizationInterface {
+    PI: {
+        HOST: string,
+        PORT: string,
+        PASSWORD: string,
+        VOLUME_STEPS: string,
+        SAVE: string,
+        AUTOMATIC_SAVE: string
+    }
+}
