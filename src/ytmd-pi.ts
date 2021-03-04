88766552f6f91ecde0e1554d3bc03158a0d96c54
XeroxDev <admin@xeroxdev.de>
2021-03-04 13:00:00 +0100

    feat: add localization support for PI

--- src/ytmd-pi.ts
+++ src/ytmd-pi.ts
@@ -1,5 +1,6 @@
 import {SDOnPiEvent, StreamDeckPropertyInspectorHandler} from 'streamdeck-typescript';
 import {ActionTypes}                                     from './interfaces/enums';
+import {LocalizationInterface}                           from './interfaces/localization.interface';
 import {PisAbstract}                                     from './pis/pis.abstract';
 import {PlayPausePi}                                     from './pis/play-pause.pi';
 import {VolumeChangePi}                                  from './pis/volume-change.pi';
@@ -11,8 +12,41 @@ export class YTMDPi extends StreamDeckPropertyInspectorHandler {
         super();
     }
 
+    // Load the localizations
+    public getLocalization(inLanguage: string, inCallback: (b: boolean, s: string | LocalizationInterface) => void) {
+        const url = '' + inLanguage + '.json';
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+
+        xhr.onload = function () {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                try {
+                    const data = JSON.parse(xhr.responseText);
+                    const localization = data['Localization'];
+                    inCallback(true, localization);
+                } catch (e) {
+                    inCallback(false, 'Localizations is not a valid json.');
+                }
+            } else {
+                inCallback(false, 'Could not load the localizations.');
+            }
+        };
+
+        xhr.onerror = function () {
+            inCallback(false, 'An error occurred while loading the localizations.');
+        };
+
+        xhr.ontimeout = function () {
+            inCallback(false, 'Localization timed out.');
+        };
+
+        xhr.send();
+    }
+
     @SDOnPiEvent('setupReady')
     private documentLoaded() {
+        this.getLocalization(
+            this.info.application.language, (b, s) => this.setupLocalization(b, s as LocalizationInterface));
         const _action: ActionTypes = this.actionInfo.action as ActionTypes;
         switch (_action) {
             case ActionTypes.PLAY_PAUSE:
@@ -25,6 +59,25 @@ export class YTMDPi extends StreamDeckPropertyInspectorHandler {
                 this.action = new VolumeChangePi(this, this.actionInfo.context, 'DOWN');
                 break;
         }
+    }
+
+    private setupLocalization(succeed: boolean, status: LocalizationInterface) {
+        if (!succeed)
+            return;
+
+        this.setLanguage('host-label', status.PI.HOST);
+        this.setLanguage('port-label', status.PI.PORT);
+        this.setLanguage('password-label', status.PI.PASSWORD);
+        this.setLanguage('save-label', status.PI.SAVE);
+        this.setLanguage('volume-steps-label', status.PI.VOLUME_STEPS);
+        this.setLanguage('automatic-save-label', status.PI.AUTOMATIC_SAVE);
+    }
+
+    private setLanguage(clzz: string, text: string) {
+        const element = document.getElementsByClassName(clzz);
+        if (element)
+            for (let i = 0; i < element.length; i++)
+                (<HTMLElement>element.item(i)).innerText = text;
     }
 
     @SDOnPiEvent('didReceiveGlobalSettings')
