3ddd44a64adf595f1bd3e40ec16b9cde519cc074
XeroxDev <admin@xeroxdev.de>
2021-03-23 18:12:30 +0100

    feat: add shuffle action

---
+++ src/actions/shuffle.action.ts
@@ -0,0 +1,23 @@
+import { WillAppearEvent, KeyUpEvent, WillDisappearEvent, SDOnActionEvent } from 'streamdeck-typescript';
+import { ActionTypes } from '../interfaces/enums';
+import { YTMD } from '../ytmd';
+import { DefaultAction } from './default.action';
+
+export class ShuffleAction extends DefaultAction<ShuffleAction> {
+    constructor(private plugin: YTMD, actionName: ActionTypes) {
+        super(plugin, actionName)
+    }
+    
+    onContextAppear(event: WillAppearEvent<any>): void {
+    }
+
+    @SDOnActionEvent('keyUp')
+    onKeypressUp(event: KeyUpEvent<any>): void {
+        this.socket.playerShuffle();
+        this.plugin.showOk(event.context);
+    }
+    
+    onContextDisappear(event: WillDisappearEvent<any>): void {
+    }
+
+}
