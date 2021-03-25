32ae68dfb330d5ee9a3bd7c2e18a0b3b329ae90e
XeroxDev <admin@xeroxdev.de>
2021-03-25 22:54:49 +0100

    refactor: split methods for better handling

--- src/helper/thumbnail.helper.ts
+++ src/helper/thumbnail.helper.ts
@@ -14,8 +14,11 @@ export class ThumbnailHelper {
         return this._instance;
     }
 
-    private _matrix: (ActionType | null)[][] = [];
+    private _matrix: (Omit<ActionType, 'action'> | null)[][] = [];
+
     private _lastCover = '';
+
+    private _imageCache: HTMLImageElement | null = null;
 
     public addAction(action: ActionType) {
         const { row, column } = action.pos;
@@ -32,8 +35,10 @@ export class ThumbnailHelper {
                 this._matrix[i] = new Array(biggestY).fill(null);
             }
         }
-        this._matrix[row][column] = action;
-        this._lastCover = '';
+        this._matrix[row][column] = {
+            pos: action.pos,
+            context: action.context,
+        };
     }
 
     public removeAction(action: ActionType) {
@@ -42,21 +47,25 @@ export class ThumbnailHelper {
     }
 
     public async setImage(cover: string) {
-        if (this._lastCover !== cover) {
+        if (cover !== this._lastCover) {
             this._lastCover = cover;
-            await this.generateThumbnails(cover);
+            this._imageCache = null;
         }
+        return await this.generateThumbnails(cover);
     }
 
     private async generateThumbnails(cover: string) {
         // TODO: Generate thumbnails for the perfect squared size
-        for (const matrix of this._matrix) {
-            for (const item of matrix) {
-                if (!item) continue;
-                const { context, action, pos } = item;
-                this.setImageFromUrl(this._lastCover, context, action, pos, 1);
-            }
-        }
+        // for (const matrix of this._matrix) {
+        //     for (const item of matrix) {
+        //         if (!item) continue;
+        //         const { context, pos } = item;
+        //         this.setImageFromUrl(this._lastCover, context, pos, 1);
+        //     }
+        // }
+
+        // Hardcoded until working (so the action has always a image)
+        return this.getImageTile(cover, 1, 0);
     }
 
     /**
@@ -67,65 +76,73 @@ export class ThumbnailHelper {
      * @param {StateType} state
      * @returns {Promise<string>}
      */
-    private setImageFromUrl(
+    private getImageTile(
         url: string,
-        context: string,
-        action: DefaultAction<any>,
-        pos: Coordinates,
-        dimension: number
+        dimension: number,
+        tileIndex: number
     ): Promise<string> {
         return new Promise((resolve, reject) => {
-            let image = new Image();
-            image.onload = () => {
-                let canvas = document.createElement('canvas');
-                canvas.width = image.naturalWidth;
-                canvas.height = image.naturalHeight;
+            if (!this._imageCache) {
+                let image = new Image();
+                image.onload = () => {
+                    const calculated = this.calculateImageSize(
+                        dimension,
+                        tileIndex
+                    );
+                    if (calculated) resolve(calculated);
+                    else reject('Could not get image');
+                };
+                image.onerror = () => {
+                    image.onload = null;
+                    image.onerror = null;
+                    (image as any) = null;
 
-                //TODO: This should calculate the right size and cut it into the pieces for each action. If it's a single key, show full image
-
-                const tileIndex = 0;
-                const tileWidth = canvas.width / dimension;
-                const tileHeight = canvas.height / dimension;
-                const tilePerLine = dimension;
-                const offsetX = (tileIndex % tilePerLine) * tileWidth;
-                const offsetY =
-                    Math.floor(tileIndex / tilePerLine) * tileHeight;
-
-                let ctx = canvas.getContext('2d');
-                if (!ctx) {
                     reject(new Error('image failed to load'));
-                    return;
-                }
-
-                ctx.drawImage(
-                    image,
-                    offsetX,
-                    offsetY,
-                    tileWidth,
-                    tileHeight,
-                    0,
-                    0,
-                    544,
-                    544
+                };
+                image.src = url;
+                this._imageCache = image;
+            } else {
+                const calculated = this.calculateImageSize(
+                    dimension,
+                    tileIndex
                 );
-
-                image.onload = null;
-                image.onerror = null;
-                (image as any) = null;
-
-                const dataUrl = canvas.toDataURL('image/png');
-
-                action.plugin.setImage(dataUrl, context);
-                resolve(dataUrl);
-            };
-            image.onerror = () => {
-                image.onload = null;
-                image.onerror = null;
-                (image as any) = null;
-
-                reject(new Error('image failed to load'));
-            };
-            image.src = url;
+                if (calculated) resolve(calculated);
+                else reject('Could not get image');
+            }
         });
+    }
+
+    private calculateImageSize(dimension: number, tileIndex: number) {
+        if (!this._imageCache) return null;
+        const image = this._imageCache;
+        let canvas = document.createElement('canvas');
+        canvas.width = image.naturalWidth;
+        canvas.height = image.naturalHeight;
+
+
+        const tileWidth = canvas.width / dimension;
+        const tileHeight = canvas.height / dimension;
+        const tilePerLine = dimension;
+        const offsetX = (tileIndex % tilePerLine) * tileWidth;
+        const offsetY = Math.floor(tileIndex / tilePerLine) * tileHeight;
+
+        let ctx = canvas.getContext('2d');
+        if (!ctx) {
+            return null;
+        }
+
+        ctx.drawImage(
+            image,
+            offsetX,
+            offsetY,
+            tileWidth,
+            tileHeight,
+            0,
+            0,
+            544,
+            544
+        );
+
+        return canvas.toDataURL('image/png');
     }
 }
