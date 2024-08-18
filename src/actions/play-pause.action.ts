6a83044e43c655cef352d6b467c11e09705fd2e1
XeroxDev <admin@xeroxdev.de>
2024-08-18 17:11:29 +0200

    revert: "feat: add streamdeck+ support" (#135)
    This reverts commit 040c3a524e015dfc11c1fe9df6fe10191ff41bf8.

--- src/actions/play-pause.action.ts
+++ src/actions/play-pause.action.ts
@@ -1,6 +1,4 @@
 import {
-    DialUpEvent,
-    DialRotateEvent,
     DidReceiveSettingsEvent,
     KeyUpEvent,
     SDOnActionEvent,
@@ -12,7 +10,6 @@ import {YTMD} from '../ytmd';
 import {DefaultAction} from './default.action';
 import {PlayPauseSettings} from "../interfaces/context-settings.interface";
 import {SocketState, StateOutput, TrackState} from "ytmdesktop-ts-companion";
-const { createCanvas, loadImage } = require('canvas');
 
 export class PlayPauseAction extends DefaultAction<PlayPauseAction> {
     private trackState: TrackState = TrackState.UNKNOWN;
@@ -25,10 +22,7 @@ export class PlayPauseAction extends DefaultAction<PlayPauseAction> {
         onError: (error: any) => void,
         onConChange: (state: SocketState) => void
     }[] = [];
-    private currentThumbnail: string;
-    private thumbnail: string;
-    private ticks = 0;
-    private lastcheck = 0;
+
 
     constructor(private plugin: YTMD, actionName: string) {
         super(plugin, actionName);
@@ -77,28 +71,7 @@ export class PlayPauseAction extends DefaultAction<PlayPauseAction> {
 
         found = {
             context: event.context,
-            onTick: (state: StateOutput) => {
-                this.handlePlayerData(event, state);
-                if (this.lastcheck === 0 && this.ticks !== 0)
-                    {
-                    if (this.ticks > 0) this.rest.next().catch(reason => {
-                        console.error(reason);
-                        this.plugin.logMessage(`Error while next. event: ${JSON.stringify(event)}, error: ${JSON.stringify(reason)}`);
-                        this.plugin.showAlert(event.context)
-                    })
-                    else this.rest.previous().catch(reason => {
-                        console.error(reason);
-                        this.plugin.logMessage(`Error while previous. event: ${JSON.stringify(event)}, error: ${JSON.stringify(reason)}`);
-                        this.plugin.showAlert(event.context)
-                    })
-                    this.ticks = 0;
-                    this.lastcheck = 3;
-                }
-                if (this.lastcheck > 0)
-                {
-                    this.lastcheck -= 1;
-                }
-            },
+            onTick: (state: StateOutput) => this.handlePlayerData(event, state),
             onConChange: (state: SocketState) => {
                 switch (state) {
                     case SocketState.CONNECTED:
@@ -136,7 +109,7 @@ export class PlayPauseAction extends DefaultAction<PlayPauseAction> {
     }
 
     @SDOnActionEvent('keyUp')
-    onKeypressUp({context, payload: {settings}}: KeyUpEvent) {
+    onKeypressUp({context, payload: {settings}}: KeyUpEvent<PlayPauseSettings>) {
         if (!settings?.action) {
             this.rest.playPause().catch(reason => {
                 console.error(reason);
@@ -168,6 +141,7 @@ export class PlayPauseAction extends DefaultAction<PlayPauseAction> {
                 });
                 break;
         }
+
         this.plugin.setState(this.trackState === TrackState.PLAYING ? StateType.ON : StateType.OFF, context);
     }
 
@@ -184,23 +158,11 @@ export class PlayPauseAction extends DefaultAction<PlayPauseAction> {
         let remaining = duration - current;
 
         const title = this.formatTitle(current, duration, remaining, context, settings);
-        const cover = this.getSongCover(data);
 
         if (this.currentTitle !== title || this.firstTimes >= 1) {
             this.firstTimes--;
             this.currentTitle = title;
             this.plugin.setTitle(this.currentTitle, context);
-            this.plugin.setFeedback(context, {"icon": this.thumbnail, "value": this.currentTitle, "indicator": { "value": current / duration * 100, "enabled": true}});
-            if (this.currentThumbnail !== cover)
-            {
-                this.currentThumbnail = cover;
-                const canvas = createCanvas(48, 48);
-                const ctx = canvas.getContext('2d');
-                loadImage(cover).then((image: any) => {
-                    ctx.drawImage(image, 0, 0, 48, 48)
-                    this.thumbnail = canvas.toDataURL('image/png');
-                });
-            }
         }
 
         if (this.trackState !== data.player.trackState) {
@@ -210,24 +172,6 @@ export class PlayPauseAction extends DefaultAction<PlayPauseAction> {
                 context
             );
         }
-    }
-
-    private getSongCover(data: StateOutput): string {
-        let cover = "";
-
-        if (!data.player || !data.video) return cover;
-
-        const trackState = data.player.trackState;
-
-        switch (trackState) {
-            case TrackState.PLAYING:
-                cover = data.video.thumbnails[data.video.thumbnails.length - 1].url ?? cover;
-                break;
-            default:
-                break;
-        }
-
-        return cover;
     }
 
     private formatTitle(current: number, duration: number, remaining: number, context: string, settings: PlayPauseSettings): string {
@@ -259,20 +203,5 @@ export class PlayPauseAction extends DefaultAction<PlayPauseAction> {
     @SDOnActionEvent('didReceiveSettings')
     private handleSettings(e: DidReceiveSettingsEvent<PlayPauseSettings>) {
         this.contextFormat[e.context] = e.payload.settings?.displayFormat ?? this.contextFormat[e.context];
-    }
-
-    @SDOnActionEvent('dialUp')
-    onDialUp({context, payload: {settings}}: DialUpEvent<PlayPauseSettings>) {
-        this.rest.playPause().catch(reason => {
-            console.error(reason);
-            this.plugin.logMessage(`Error while playPause toggle. context: ${JSON.stringify(context)}, error: ${JSON.stringify(reason)}`);
-            this.plugin.showAlert(context)
-        });
-        this.plugin.setState(this.trackState === TrackState.PLAYING ? StateType.ON : StateType.OFF, context);
-    }
-
-    @SDOnActionEvent('dialRotate')
-    onDialRotate({context, payload: {settings, ticks}}: DialRotateEvent) {
-        this.ticks += ticks;
     }
 }
--- src/actions/vol-change.action.ts
+++ src/actions/vol-change.action.ts
@@ -1,4 +1,4 @@
-import {KeyDownEvent, KeyUpEvent, SDOnActionEvent, WillAppearEvent, WillDisappearEvent, DialRotateEvent, DialUpEvent} from 'streamdeck-typescript';
+import {KeyDownEvent, KeyUpEvent, SDOnActionEvent, WillAppearEvent, WillDisappearEvent,} from 'streamdeck-typescript';
 import {YTMD} from '../ytmd';
 import {DefaultAction} from './default.action';
 import {StateOutput} from "ytmdesktop-ts-companion";
@@ -7,10 +7,7 @@ export class VolChangeAction extends DefaultAction<VolChangeAction> {
     private keyDown: boolean = false;
     private currentVolume: number = 50;
     private events: { context: string, method: (state: StateOutput) => void }[] = [];
-    private lastVolume = 0;
-    private ticks = 0;
-    private lastcheck = 0;
-    private iconpath: string;
+
 
     constructor(
         private plugin: YTMD,
@@ -21,60 +18,20 @@ export class VolChangeAction extends DefaultAction<VolChangeAction> {
     }
 
     @SDOnActionEvent('willAppear')
-    onContextAppear({context, payload: {settings}}: WillAppearEvent): void {
-        let found = this.events.find(e => e.context === context);
+    onContextAppear(event: WillAppearEvent): void {
+        let found = this.events.find(e => e.context === event.context);
         if (found) {
             return;
         }
 
         found = {
-            context: context,
-            method: (state: StateOutput) => { 
-                this.currentVolume = state.player.volume;
-                this.updateIcon();
-                this.plugin.setFeedback(context, {"icon": this.iconpath, "title": "Volume", "value": this.currentVolume + "%", "indicator": { "value": this.currentVolume, "enabled": true}});
-                if (this.lastcheck === 0 && this.ticks !== 0)
-                {
-                    let newVolume = this.currentVolume + this.lastVolume;
-                    this.lastVolume = 0;
-                    newVolume += (settings?.steps ?? 2) * this.ticks;
-            
-                    this.rest.setVolume(newVolume < 0 ? 0 : newVolume > 100 ? 100 : newVolume).catch(reason => {
-                        newVolume = this.currentVolume;
-                        console.error(reason);
-                        this.plugin.logMessage(`Error while setting volume. volume: ${newVolume}, context: ${JSON.stringify(context)}, error: ${JSON.stringify(reason)}`);
-                        this.plugin.showAlert(context)
-                    }).finally(() => {
-                        this.currentVolume = newVolume;
-                    });
-                    this.ticks = 0
-                    this.lastcheck = 3;
-                }
-                if (this.lastcheck > 0)
-                {
-                    this.lastcheck -= 1;
-                }
-            }
+            context: event.context,
+            method: (state: StateOutput) => this.currentVolume = state.player.volume
         };
 
         this.events.push(found);
 
         this.socket.addStateListener(found.method);
-    }
-
-    private updateIcon(): void {
-        if (this.currentVolume >= 66) {
-            this.iconpath = "icons/volume-up";
-        }
-        else if (this.currentVolume >= 33) {
-            this.iconpath = "icons/volume-on";
-        }
-        else if (this.currentVolume <= 0) {
-            this.iconpath = "icons/volume-mute";
-        }
-        else {
-            this.iconpath = "icons/volume-down";
-        }
     }
 
     @SDOnActionEvent('willDisappear')
@@ -110,27 +67,6 @@ export class VolChangeAction extends DefaultAction<VolChangeAction> {
             });
             await this.wait(500);
         }
-    }
-
-    @SDOnActionEvent('dialUp')
-    onDialUp({context, payload: {settings}}: DialUpEvent) {
-        if (this.currentVolume <= 0) {
-            this.currentVolume = this.lastVolume;
-            this.lastVolume = 0;
-        } else {
-            this.lastVolume = this.currentVolume;
-            this.currentVolume = 0;
-        }
-        this.rest.setVolume(this.currentVolume).catch(reason => {
-            console.error(reason);
-            this.plugin.logMessage(`Error while setting volume. volume: ${this.currentVolume}, context: ${JSON.stringify(context)}, error: ${JSON.stringify(reason)}`);
-            this.plugin.showAlert(context)
-        });
-    }
-
-    @SDOnActionEvent('dialRotate')
-    onDialRotate({context, payload: {settings, ticks}}: DialRotateEvent) {
-        this.ticks += ticks;
     }
 
     private wait(ms: number): Promise<void> {
